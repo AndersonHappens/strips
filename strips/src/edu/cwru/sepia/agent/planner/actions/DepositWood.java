8760540a58e613d710a0636c4f56552282391748
Joseph Tate <jgt17@case.edu>
2015-04-05 15:09:20 -0400

    added depositWood to index

---
+++ strips/src/edu/cwru/sepia/agent/planner/actions/DepositWood.java
@@ -0,0 +1,71 @@
+package edu.cwru.sepia.agent.planner.actions;
+
+import java.util.ArrayList;
+
+import edu.cwru.sepia.action.Action;
+import edu.cwru.sepia.agent.planner.GameState;
+import edu.cwru.sepia.agent.planner.Position;
+
+
+public class DepositWood implements StripsAction {
+     
+     private Integer[] peasantIdsInvolved;
+     private Position[] peasantPositionsInvolved;
+     private int peasantsInvolved;
+     private Position townHallPosition;
+     
+     public DepositWood(int k) {
+          peasantsInvolved=k;
+          peasantIdsInvolved=new Integer[peasantsInvolved];
+          peasantPositionsInvolved=new Position[peasantsInvolved];
+          townHallPosition=null;
+     }
+
+     @Override
+     public boolean preconditionsMet(GameState state) {
+          Integer[] peasantIDs=state.getPeasantIds().toArray(new Integer[0]);
+          Position[] peasantPositions=state.getPeasantPositions().toArray(new Position[0]);
+          Integer[] peasantCargo=state.getPeasantCargo().toArray(new Integer[0]);
+          townHallPosition=state.getTownHallPosition();
+          int peasantsAvailable=0;
+          for(int i=0;i<peasantPositions.length && peasantsInvolved>peasantsAvailable;i++) {
+               if(peasantPositions[i].isAdjacent(townHallPosition) && peasantCargo[i].intValue()==GameState.WOOD) {
+                    peasantIdsInvolved[peasantsAvailable]=peasantIDs[i];
+                    peasantPositionsInvolved[peasantsAvailable]=peasantPositions[i];
+               }
+          }
+          if(peasantsAvailable>=peasantsInvolved) {
+               return true;
+          } else {
+               return false;
+          }
+     }
+
+     @Override
+     public GameState apply(GameState state) {
+          GameState newState=state.copyOf();
+          Integer[] peasantIDs=state.getPeasantIds().toArray(new Integer[0]);
+          ArrayList<Integer> peasantCargo=state.getPeasantCargo();
+          for(int i=0;i<peasantIDs.length;i++) {
+               for(int j=0;j<peasantIdsInvolved.length;j++) {
+                    if(peasantIDs[i].equals(peasantIdsInvolved.length)) {
+                         peasantCargo.set(i, GameState.NONE);
+                    }
+               }
+          }
+          newState.setWoodAmount(newState.getWoodAmount()+peasantsInvolved*100);
+          newState.setPeasantCargo(peasantCargo);
+          newState.setParent(state);
+          newState.setAction(this);
+          return null;
+     }
+
+     @Override
+     public ArrayList<Action> toSepiaAction() {
+          ArrayList<Action> actions=new ArrayList<Action>();
+          for(int i=0;i<peasantIdsInvolved.length;i++) {
+               actions.add(Action.createPrimitiveDeposit(peasantIdsInvolved[i], peasantPositionsInvolved[i].getDirection(townHallPosition)));   
+          }
+          return actions;
+     }
+}
